42469847a8cea418053e412d87828ab5419c2d37
Jun <jun55tsuno@gmail.com>
2022-12-23 14:12:02 -0800

    finished

---
+++ src/components/MainPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MainPage = () => {
+	return (
+		<div className="mainPage">
+			<h1>Welcome to the Website!!</h1>
+			<button>Click here!!</button>
+		</div>
+	);
+};
+
+export default MainPage;
